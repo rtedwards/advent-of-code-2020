a36bf7ef8cb22fa401d3c4b17050555e10722a3b
Robert <rtedwards.eng@gmail.com>
2020-12-06 11:15:04 +0000

    cleaned up day 6

--- day-6.jl
+++ day-6.jl
@@ -4,26 +4,21 @@ function main_6()
     ans = read("day-6.txt", String)
     groups = split(ans, "\n\n")
 
-    total_answers = 0
-    everyone_answered = 0
+    total, every = 0, 0
     for group in groups
-        peeps = split(group, "\n")
-        n_peeps = length(peeps)
-        
+        peeps = split(group, "\n")        
         hist = Dict()
         for person in peeps
             for l in person
                 hist[l] = get!(hist, l, 0) + 1
             end
         end
-        a = values(hist)
-        l = collect(a)[a .== n_peeps]
-        everyone_answered += length(l)
-        total_answers += sum(length(keys(hist)))
+        l = filter(x -> x .== length(peeps), collect(values(hist)))
+        total += sum(length(keys(hist)))
+        every += length(l)
     end
 
-    println("There are $total_answers unique answers")
-    println("Everyone_answered $everyone_answered questions")
+    println("$total unique answers, $every questions answered by everyone")
 end
 
 main_6()
